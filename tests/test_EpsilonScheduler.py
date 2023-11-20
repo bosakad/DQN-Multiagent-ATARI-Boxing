fa10ddee698678a932df417e3122983a2df5039d
Adam Bosak <bosadam@seznam.cz>
2023-11-20 16:40:06 -0500

    created packages and tests for epsilon scheduler

--- tests/test_EpsilonScheduler.py
+++ tests/test_EpsilonScheduler.py
@@ -1,30 +1,35 @@
-import EpsilonScheduler
+from src.EpsilonScheduler import EpsilonScheduler
 
-def _test_scheduler(schedule, step, value, ndigits=5):
-    """Tests that the schedule returns the correct value."""
-    v = schedule.value(step)
-    if not round(v, ndigits) == round(value, ndigits):
-        raise Exception(
-            f'For step {step}, the scheduler returned {v} instead of {value}'
-        )
+import unittest
 
+class TestEpsilonScheduler(unittest.TestCase):
 
-_schedule = EpsilonScheduler(0.1, 0.2, 3)
-_test_scheduler(_schedule, -1, 0.1)
-_test_scheduler(_schedule, 0, 0.1)
-_test_scheduler(_schedule, 1, 0.141421356237309515)
-_test_scheduler(_schedule, 2, 0.2)
-_test_scheduler(_schedule, 3, 0.2)
-del _schedule
+    # helper function to test the scheduler
+    def _test_scheduler(self, schedule, step, value, ndigits=5):
+        """Tests that the schedule returns the correct value."""
+        v = schedule.value(step)
+        assert round(v, ndigits) == round(value, ndigits), f'For step {step}, the scheduler returned {v} instead of {value}'
 
-_schedule = EpsilonScheduler(0.5, 0.1, 5)
-_test_scheduler(_schedule, -1, 0.5)
-_test_scheduler(_schedule, 0, 0.5)
-_test_scheduler(_schedule, 1, 0.33437015248821106)
-_test_scheduler(_schedule, 2, 0.22360679774997905)
-_test_scheduler(_schedule, 3, 0.14953487812212207)
-_test_scheduler(_schedule, 4, 0.1)
-_test_scheduler(_schedule, 5, 0.1)
-del _schedule
+    # test to be called by unittest
+    def test_scheduler(self):
+        _schedule = EpsilonScheduler(0.1, 0.2, 3)
+        self._test_scheduler(_schedule, -1, 0.1)
+        self._test_scheduler(_schedule, 0, 0.1)
+        self._test_scheduler(_schedule, 1, 0.141421356237309515)
+        self._test_scheduler(_schedule, 2, 0.2)
+        self._test_scheduler(_schedule, 3, 0.2)
+        del _schedule
 
-print("tests Passed!")
+        _schedule = EpsilonScheduler(0.5, 0.1, 5)
+        self._test_scheduler(_schedule, -1, 0.5)
+        self._test_scheduler(_schedule, 0, 0.5)
+        self._test_scheduler(_schedule, 1, 0.33437015248821106)
+        self._test_scheduler(_schedule, 2, 0.22360679774997905)
+        self._test_scheduler(_schedule, 3, 0.14953487812212207)
+        self._test_scheduler(_schedule, 4, 0.1)
+        self._test_scheduler(_schedule, 5, 0.1)
+        del _schedule
+
+if __name__ == '__main__':
+
+    unittest.main()
