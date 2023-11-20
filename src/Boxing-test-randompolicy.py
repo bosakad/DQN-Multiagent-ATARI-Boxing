f2de9fe51ea02a0fda2aed0c937d1f11c2996d9e
Adam Bosak <bosadam@seznam.cz>
2023-11-20 18:10:43 -0500

    Update Boxing-test-randompolicy.py

--- src/Boxing-test-randompolicy.py
+++ src/Boxing-test-randompolicy.py
@@ -3,9 +3,13 @@ from pettingzoo.atari import boxing_v2
 env = boxing_v2.parallel_env(render_mode="human")
 observations, infos = env.reset()
 
+print(observations)
+
 while env.agents:
     # this is where you would insert your policy
     actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+    
 
     observations, rewards, terminations, truncations, infos = env.step(actions)
+
 env.close()
