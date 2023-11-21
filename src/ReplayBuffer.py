90fb57f23fbddcd466d7f399a30401411bfd1a25
Adam Bosak <bosadam@seznam.cz>
2023-11-20 20:45:53 -0500

    torch buffer done

---
+++ src/ReplayBuffer.py
@@ -0,0 +1,24 @@
+import random
+from collections import namedtuple, deque
+
+
+# define a named tuple to store the transition
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+    """Replay Memory that stores the transitions"""
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
