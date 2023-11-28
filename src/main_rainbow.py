479f7de74ed5cd4549aa217ed97437767543e9b8
Adam Bosak <bosadam@seznam.cz>
2023-11-27 20:51:13 -0500

    deleted cart pole

--- src/main_rainbow.py
+++ src/main_rainbow.py
@@ -29,9 +29,9 @@ def train_boxing():
     seed_torch(SEED)
     
     # parameters
-    num_frames = 10000
+    num_frames = 1000
     memory_size = 10000
-    batch_size = 6
+    batch_size = 32
     target_update = 100
     
     agents = Atari_Agents(env, memory_size, batch_size, target_update, SEED)
@@ -47,38 +47,6 @@ def train_boxing():
     agents.test(video_folder, env)
 
     env.close()
-
-
-def train_cartPole():
-    # environment
-    env = gym.make("CartPole-v1", max_episode_steps=200, render_mode="rgb_array")
-
-    seed = 777
-
-    def seed_torch(seed):
-        torch.manual_seed(seed)
-        if torch.backends.cudnn.enabled:
-            torch.cuda.manual_seed(seed)
-            torch.backends.cudnn.benchmark = False
-            torch.backends.cudnn.deterministic = True
-
-    np.random.seed(seed)
-    random.seed(seed)
-    seed_torch(seed)
-
-    # parameters
-    num_frames = 1000
-    memory_size = 10000
-    batch_size = 128
-    target_update = 100
-
-    # train
-    agents = Atari_Agents(env, memory_size, batch_size, target_update, seed)
-
-    agents.train(num_frames)
-
-    video_folder="../results/videos/rainbow"
-    agents.test(video_folder=video_folder)
 
 
 if __name__ == "__main__":
